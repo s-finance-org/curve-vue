a99e9c2d8b083abae47dac08365d9f13e5e7094e
sunbeyond <sunbeyond@gmail.com>
2020-10-29 11:13:31 +0800

    fix dForce get_dy_underlying

--- src/components/swap/Swap.vue
+++ src/components/swap/Swap.vue
@@ -933,7 +933,7 @@
                   let calls = [
                     [currentContract.swap._address, currentContract.swap.methods.balances(i).encodeABI()],
                   ]
-                  if(!this.swapwrapped && !['susdv2', 'tbtc', 'ren', 'dusd', 'okuu'].includes(this.currentPool))
+                  if(!this.swapwrapped && !['susdv2', 'tbtc', 'ren', 'okuu'].includes(this.currentPool))
                     calls.push([currentContract.swap._address, currentContract.swap.methods.get_dy_underlying(i, j, dx).encodeABI()])
                   else {
                     //dx = cBN(dx).times(currentContract.c_rates[i])
